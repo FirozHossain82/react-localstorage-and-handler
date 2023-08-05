085666fc70e76e3df41ce222870bbdd71f13d1f5
FirozHossain82 <firozhossain295@gmail.com>
2023-08-05 16:13:15 +0600

    onClick functon handeler wrapper added

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Cosmetic = (props) => {
-    const{name, price, id} = props.cosmetic;
-    return (
-        <div style={{border:'3px solid red',margin:'30px',padding:'15px', borderRadius:'9px', background:'' }}>
-            <h1>Product Name: {name}</h1>
-            <h3>Price: {price}</h3>
-            <p><small>Id Is: {id}</small></p>
-        </div>
-    );
+  const { name, price, id } = props.cosmetic;
+  const addToCart = (id) =>{
+    console.log('item added', id)
+  }
+  return (
+    <div
+      style={{
+        border: "3px solid red",
+        margin: "30px",
+        padding: "15px",
+        borderRadius: "9px",
+        background: "",
+      }}
+    >
+      <h1>Product Name: {name}</h1>
+      <h3>Price: ${price}</h3>
+      <p>
+        <small>Id Is: {id}</small>
+      </p>
+      <button onClick={() => addToCart(id)} style={{border:'2px solid green',padding:'7px',borderRadius:'2px'}}>Add To Cart</button>
+    </div>
+  );
 };
 
 export default Cosmetic;
